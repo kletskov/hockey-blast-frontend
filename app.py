d819f4430beb4d292c45fc5570ae97b8e8c0611a
Pasha <kletskov@gmail.com>
2025-01-07 15:10:28 -0800

    Change the http port back to default

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context():
         db.create_all()  # Create database tables for all models
-    Thread(target=run_http, args=(app, 5009)).start()
+    Thread(target=run_http, args=(app, 5000)).start()
