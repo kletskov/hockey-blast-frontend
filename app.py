c1a0567369ed8d0abd6f3ab4b893dc44cdc25d5e
Pasha <kletskov@gmail.com>
2025-02-08 09:32:17 -0800

    Added about endpoint

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ from blueprints.game_shootout import game_shootout_bp
 from blueprints.version import version_bp
 from blueprints.dropdowns import dropdowns_bp
 from blueprints.games import games_bp
+from blueprints.about import about_bp
 
 # BLOCKED_USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.6834.83 Mobile Safari/537.36 (compatible; GoogleOther)"
 # BLOCKED_IPS = ["66.249.72.103", "66.249.72.204"]
@@ -89,6 +90,7 @@ def create_app(db_name):
     app.register_blueprint(version_bp)
     app.register_blueprint(games_bp, url_prefix='/games')
     app.register_blueprint(dropdowns_bp, url_prefix='/dropdowns')
+    app.register_blueprint(about_bp)
     
     # @app.before_request
     # def before_request():
@@ -165,6 +167,10 @@ def create_app(db_name):
     @app.route('/robots.txt')
     def robots_txt():
         return send_from_directory(app.root_path, 'robots.txt')
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
 
     return app
 
