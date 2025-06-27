fd7c9fdec79f196f0ec7c3b6050fc09437552bbf
Pasha <kletskov@gmail.com>
2025-06-27 16:01:35 -0700

    flexible flask table import patching

--- patch_flask_table.sh
+++ patch_flask_table.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Locate the install path dynamically
+SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[0])")
+FLASK_TABLE_DIR="$SITE_PACKAGES/flask_table"
+
 # Patch flask_table to use Markup from markupsafe
-sed -i 's/from flask import Markup, url_for/from markupsafe import Markup\nfrom flask import url_for/' /usr/local/lib/python3.9/site-packages/flask_table/columns.py
-sed -i 's/from flask import Markup/from markupsafe import Markup/' /usr/local/lib/python3.9/site-packages/flask_table/html.py
-sed -i 's/from flask import Markup/from markupsafe import Markup/' /usr/local/lib/python3.9/site-packages/flask_table/table.py
+sed -i 's/from flask import Markup, url_for/from markupsafe import Markup\nfrom flask import url_for/' "$FLASK_TABLE_DIR/columns.py"
+sed -i 's/from flask import Markup/from markupsafe import Markup/' "$FLASK_TABLE_DIR/html.py"
+sed -i 's/from flask import Markup/from markupsafe import Markup/' "$FLASK_TABLE_DIR/table.py"
